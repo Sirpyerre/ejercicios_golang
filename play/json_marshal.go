b7d02e42dadce5bb5d95c58fc524941e6eb44195
Pedro Rojas Reyes <pedro.rojas@gmail.com>
2020-04-30 10:06:44 -0500

    json marshal

---
+++ play/json_marshal.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type persona struct {
+	Nombre   string
+	Apellido string
+	Edad     int
+}
+
+func main() {
+	p1 := persona{
+		Nombre:   "James",
+		
+		Apellido: "Bond",
+		Edad:     32,
+	}
+
+	p2 := persona{
+		Nombre:   "Miss",
+		Apellido: "Moneypenny",
+		Edad:     32,
+	}
+
+	personas := []persona{p1, p2}
+	fmt.Println(personas)
+
+	bs, err := json.Marshal(personas)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(bs))
+}
